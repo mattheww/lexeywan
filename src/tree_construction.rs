4d1ee3b9961a0fc0c1dc002f44f7517f0029aa85
Matthew Woodcraft <matthew@woodcraft.me.uk>
2025-09-19 16:50:09 +0100

    tree_construction: make the implementation more like a traditional hand-rolled parser
    No change in behaviour intended

--- src/tree_construction.rs
+++ src/tree_construction.rs
@@ -14,28 +14,47 @@ pub trait Token {
 pub fn construct_forest<TOKEN: Token>(
     tokens: impl IntoIterator<Item = TOKEN>,
 ) -> Result<Forest<TOKEN>, String> {
-    construct_forest_inner(&mut tokens.into_iter(), None)
+    match construct_forest_inner(&mut tokens.into_iter())? {
+        (forest, None) => Ok(forest),
+        (_, Some(closing_group_kind)) => Err(format!(
+            "unexpected close delimiter: {}",
+            closing_group_kind.close_char()
+        )),
+    }
 }
 
+/// Converts tokens until end-of-stream or an unexpected closing delimiter.
+///
+/// Returns the converted forest and the GroupKind of the closing delimiter.
 fn construct_forest_inner<TOKEN: Token>(
     tokens: &mut impl Iterator<Item = TOKEN>,
-    in_group: Option<GroupKind>,
-) -> Result<Forest<TOKEN>, String> {
+) -> Result<(Forest<TOKEN>, Option<GroupKind>), String> {
     let mut constructed = Forest::<TOKEN>::new();
     while let Some(token) = tokens.next() {
         let tree = match token.as_delimiter() {
             Some(mark) => {
-                if let Some(group_kind) = GroupKind::for_open_char(mark) {
-                    Tree::<TOKEN>::Group(
-                        group_kind,
-                        construct_forest_inner(tokens, Some(group_kind))?,
-                    )
-                } else if let Some(group_kind) = GroupKind::for_close_char(mark) {
-                    if Some(group_kind) == in_group {
-                        return Ok(constructed);
-                    } else {
-                        return Err(format!("unexpected close delimiter: {mark}"));
+                if let Some(opening_group_kind) = GroupKind::for_open_char(mark) {
+                    match construct_forest_inner(tokens)? {
+                        (forest, Some(closing_group_kind))
+                            if closing_group_kind == opening_group_kind =>
+                        {
+                            Tree::<TOKEN>::Group(opening_group_kind, forest)
+                        }
+                        (_, Some(closing_group_kind)) => {
+                            return Err(format!(
+                                "unexpected close delimiter: {}",
+                                closing_group_kind.close_char()
+                            ));
+                        }
+                        (_, None) => {
+                            return Err(format!(
+                                "missing close delimiter: {}",
+                                opening_group_kind.close_char()
+                            ));
+                        }
                     }
+                } else if let Some(closing_group_kind) = GroupKind::for_close_char(mark) {
+                    return Ok((constructed, Some(closing_group_kind)));
                 } else {
                     Tree::<TOKEN>::Token(token)
                 }
@@ -44,11 +63,5 @@ fn construct_forest_inner<TOKEN: Token>(
         };
         constructed.push(tree);
     }
-    match in_group {
-        Some(group_kind) => Err(format!(
-            "missing close delimiter: {}",
-            group_kind.close_char()
-        )),
-        None => Ok(constructed),
-    }
+    Ok((constructed, None))
 }
