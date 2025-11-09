95ece356165f2bcb22bf35ef01137469122555d2
Matthew Woodcraft <matthew@woodcraft.me.uk>
2025-11-09 16:51:27 +0000

    framework.rs: make most submodules private

--- src/framework.rs
+++ src/framework.rs
@@ -1,9 +1,9 @@
 //! Framework and CLI for comparing lexer implementations
 
 pub mod command_line;
-pub mod proptesting;
-pub mod simple_reports;
-pub mod simple_tests;
 
 mod comparison;
+mod proptesting;
+mod simple_reports;
+mod simple_tests;
 mod testcases;
